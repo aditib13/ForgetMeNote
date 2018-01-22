1e81e6e525a41e310b999b59d4969f0a7ca73150
aditibh <aditibh@umich.edu>
2018-01-21 20:28:51 -0500

    wrote a function to get list from storage object

--- popup.js
+++ popup.js
@@ -3,6 +3,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// chrome.storage.local.clear();
 	document.getElementById('save').addEventListener('click', saveChanges);
 
+	// takes storage object and creates a list from it
+	function getListFromStorage(storageObject) {
+		var list;
+		if (Object.keys(storageObject).length === 0 && storageObject.constructor === Object) {
+			list = [];
+		} else {
+			list = storageObject[url];
+			console.log(storageObject[url], list, storageObject);
+		}
+		return list;
+	}
+
 	function printList(list) {
 		var s = '<ul>';
 		for (var i = 0; i < list.length; i++) {
@@ -11,21 +23,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 		s += '</ul>'
 		document.getElementById("notes").innerHTML = s;
-	};
+	}
 
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 		var url = tabs[0].url;
 		console.log("getNotes", url);
 		chrome.storage.local.get([url], function(n) {
-			console.log("results from get request", n);
-			console.log(n[url]);
-			var list;
-			if (Object.keys(n).length === 0 && n.constructor === Object) {
-				list = [];
-			} else {
-				list = n[url];
-				console.log(n[url], list, n);
-			}
+			var list = getListFromStorage(n);
 			printList(list);
 		});
 	});
@@ -45,26 +49,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			// add the new input to current list
 			// create storageObject with new, updated list
 			chrome.storage.local.get([url], function(n) {
-				console.log("results from get request", n);
-				console.log(n[url]);
-				var list;
-				if (Object.keys(n).length === 0 && n.constructor === Object) {
-					list = [];
-				} else {
-					list = n[url];
-					console.log(n[url], list, n);
-				}
-				
-			list.push(valueInInput);
-			console.log("list of notes", list);
+				var list = getListFromStorage(n);					
+				list.push(valueInInput);
+				console.log("list of notes", list);
 
-			printList(list);
+				printList(list);
 
-			var storageObject = {
-				[url]: list
-			};
-	
-			chrome.storage.local.set(storageObject);
+				var storageObject = {
+					[url]: list
+				};
+		
+				chrome.storage.local.set(storageObject);
 			});
 		});
 		resetForm();
